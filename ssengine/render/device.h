cfe565930d959000ca7c5c92668d103e7d7dfa16
DengYun <tdzl2003>
2015-02-22 21:17:15 +0800

    rendering

--- ssengine/render/device.h
+++ ssengine/render/device.h
@@ -26,8 +26,110 @@ enum ss_clear_flags{
 	SS_CF_STENCIL	= 0x4
 };
 
-struct ss_buffer{
-	virtual ~ss_buffer();
+enum ss_primitive_type{
+	SS_MT_NULL			= 0x0,
+	SS_MT_POINTLIST		= 0x1,
+	SS_MT_LINELIST		= 0x2,
+	SS_MT_LINESTRIP		= 0x3,
+	SS_MT_TRIANGLELIST	= 0x4,
+	SS_MT_TRIANGLESTRIP	= 0x5,
+};
+
+enum ss_render_format{
+	SS_VBF_NULL			= 0x0,
+	SS_VBF_FLOAT32_RGBA	= 0x1,
+};
+
+inline size_t ss_render_format_sizeof(ss_render_format type){
+	static size_t sizes[] = {
+		0, 16
+	};
+	int iType = (int)type;
+	if (iType >= 0 && iType <= 2){
+		return sizes[iType];
+	}
+	return 0;
+}
+
+struct ss_vertex_buffer{
+	virtual ~ss_vertex_buffer() = 0{}
+};
+
+struct ss_vertex_buffer_memory : ss_vertex_buffer{
+	virtual void* lock() = 0;
+	virtual void unlock() = 0;
+};
+
+struct ss_vertex_buffer_static : ss_vertex_buffer{
+
+};
+
+struct ss_vertex_buffer_managed : ss_vertex_buffer{
+
+};
+
+enum ss_render_input_usage_type{
+	SS_USAGE_NONE		= 0,
+	SS_USAGE_POSITION	= 1,
+	SS_USAGE_COLOR		= 2,
+	SS_USAGE_TEXCOORD	= 3,
+	SS_USAGE_NORMAL		= 4
+};
+
+struct ss_render_input_usage{
+	ss_render_input_usage_type	type;
+	unsigned int				index;
+
+	ss_render_input_usage(
+		ss_render_input_usage_type _type = SS_USAGE_NONE,
+		unsigned _index = 0)
+		: type(_type), index(_index)
+	{
+	}
+
+	bool operator == (const ss_render_input_usage& other) const{
+		return type == other.type && index == other.index;
+	}
+};
+
+struct ss_render_input_element
+{
+	ss_render_input_usage	usage;
+	unsigned int			index;		//index of array or matrix
+	ss_render_format		format;
+	unsigned int			slot;		//slot of vertex buffer
+	unsigned int			alignOffset;
+};
+
+struct ss_render_input_layout{
+	virtual ~ss_render_input_layout() = 0 {}
+};
+
+struct ss_render_pass{
+protected:
+	~ss_render_pass(){}
+public:
+	virtual void begin() = 0;
+	virtual void end() = 0;
+};
+
+struct ss_render_technique{
+	virtual ~ss_render_technique() = 0 {};
+	virtual size_t pass_count() = 0;
+	virtual ss_render_pass* get_pass(size_t index) = 0;
+
+	virtual ss_render_input_layout* create_input_layout(
+		ss_render_input_element* elements,
+		size_t		count
+		) = 0;
+
+};
+
+enum ss_predefined_technique_type{
+	SS_PDT_BLANK				= 0,
+	SS_PDT_STANDARD_NO_TEXTURE	= 1,
+	SS_PDT_STANDARD				= 2,
+	SS_PDT_GREY					= 3
 };
 
 struct ss_render_device
@@ -47,10 +149,35 @@ struct ss_render_device
 	virtual void clear(int flags = SS_CF_COLOR) = 0;
 	virtual void present() = 0;
 
-	void destroy(){delete this};
+	virtual void set_viewport(int left, int top, int width, int height) = 0;
+
+	void destroy(){ delete this; };
+
+	virtual void set_primitive_type(ss_primitive_type mt) = 0;
 
 	virtual void draw(int count, int from) = 0;
 	virtual void draw_index(int count, int from, int base) = 0;
+
+	virtual ss_vertex_buffer_memory* create_memory_vertex_bufer(
+		ss_render_format type,
+		size_t count) = 0;
+
+	virtual ss_render_technique* get_predefined_technique(ss_predefined_technique_type type) = 0;
+
+	virtual void set_input_layout(ss_render_input_layout* layout) = 0;
+
+	virtual void set_vertex_buffer(
+			size_t start,
+			size_t num,
+			ss_vertex_buffer* const * buffer,
+			const unsigned int* strides,
+			const unsigned int* offset
+		) = 0;
+
+	virtual void unset_vertex_buffer(
+		size_t start,
+		size_t num
+		) = 0;
 };
 
 typedef ss_render_device* (*ss_device_factory_type)(ss_device_type dt, uintptr_t hwnd);
