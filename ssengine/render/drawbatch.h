bc89c7ba7b5d8753169abfe965e2d5b6e4f544de
DengYun <tdzl2003>
2015-02-26 22:12:27 +0800

    implement draw batch.

--- ssengine/render/drawbatch.h
+++ ssengine/render/drawbatch.h
@@ -6,11 +6,26 @@
 extern "C" {
 #endif
 
-extern "C" void draw_line(float x0, float y0, float x1, float y1);
+typedef struct ss_core_context ss_core_context;
+
+//SS_CORE_API void ss_db_discard(ss_core_context* C);
+
+SS_CORE_API void ss_db_flush(ss_core_context* C);
+
+SS_CORE_API void ss_db_draw_line(ss_core_context* C, float x0, float y0, float x1, float y1);
 
 
 
 
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef __cplusplus
+#include <ssengine/render/types.h>
+
+inline void ss_db_draw_line(ss_core_context* C, ss::float2 from, ss::float2 to){
+	ss_db_draw_line(C, from.x, from.y, to.x, to.y);
+}
+
 #endif
--- ssengine/render/types.h
+++ ssengine/render/types.h
@@ -66,6 +66,12 @@ namespace ss{
 				float r, g, b, a;
 			};
 		};
+		color(float _r = 0.0f, float _g = 0.0f, float _b = 0.0f, float _a = 1.0f)
+			: r(_r), g(_g), b(_b), a(_a)
+		{
+
+		}
+	
 	};
 
 	struct SS_CORE_API matrix{
