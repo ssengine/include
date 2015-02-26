280f84a2e0b1842c80a3e6bfe7e061ff6930af2e
DengYun <tdzl2003>
2015-02-26 22:34:37 +0800

    add draw_image_rect

--- ssengine/render/drawbatch.h
+++ ssengine/render/drawbatch.h
@@ -7,6 +7,7 @@ extern "C" {
 #endif
 
 typedef struct ss_core_context ss_core_context;
+typedef struct ss_texture2d ss_texture2d;
 
 //SS_CORE_API void ss_db_discard(ss_core_context* C);
 
@@ -14,7 +15,12 @@ SS_CORE_API void ss_db_flush(ss_core_context* C);
 
 SS_CORE_API void ss_db_draw_line(ss_core_context* C, float x0, float y0, float x1, float y1);
 
-
+SS_CORE_API void ss_db_draw_image_rect(
+		ss_core_context* C,
+		ss_texture2d*	 texture,
+		float l, float t, float w, float h,
+		float tl, float tt, float tw, float th
+	);
 
 
 #ifdef __cplusplus
